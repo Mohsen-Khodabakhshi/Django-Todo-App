2183abf2422cbb7142935f64966da28947044689
Mohsen Khodabakhshi <mim.khodabakhshi@gmail.com>
2021-02-27 22:45:06 +0330

    some bug in authentication fixes

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,10 +1,43 @@
 from django import forms
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 class SignUpForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    def __init__(self, *args, **kwargs):
+        self.req = kwargs.pop('req', None)
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        queryset = User.objects.filter(username=username)
+        if queryset.exists():
+            messages.error(self.req, 'Username before used')
+            raise forms.ValidationError("Username before used")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        queryset = User.objects.filter(email=email)
+        if queryset.exists():
+            messages.error(self.req, 'Email before used')
+            raise forms.ValidationError("Email before used")
+        return email
+
+    def clean(self):
+        password = self.cleaned_data.get('password')
+        confirm = self.cleaned_data.get('password2')
+        if password == confirm:
+            return self.cleaned_data
+        else:
+            messages.error(self.req, 'Passwords not match!')
+            raise forms.ValidationError('Passwords not match!')
+            return redirect('/')
 
 class SignInForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+from django.contrib.auth import get_user_model, login, authenticate, logout
 
 from .forms import SignInForm, SignUpForm
-
-from django.contrib.auth import get_user_model, login, authenticate, logout
 
 # Create your views here.
 
@@ -29,7 +29,8 @@ def signIn(request):
                     login(request, user)
                     return redirect('/nd')
                 else:
-                    return HttpResponse('User not found')
+                    messages.error(request, 'User Not Found!')
+                    return redirect('/')
         else:
             return redirect('/')
     else:
@@ -39,19 +40,20 @@ User = get_user_model()
 def signUp(request):
     if not request.user.is_authenticated:
         if request.method == 'POST':
-            form = SignUpForm(request.POST)
+            form = SignUpForm(request.POST, req=request)
             if form.is_valid():
                 username = form.cleaned_data.get('username')
                 email = form.cleaned_data.get('email')
                 password = form.cleaned_data.get('password')
                 User.objects.create_user(username=username, email=email, password=password)
+                messages.error(request, '*Now you can SignIn*')
                 return redirect('/')
             return redirect('/')
     else:
         return redirect('/nd')
 
 def logOut(request):
-    if not request.user.is_authenticated:
+    if request.user.is_authenticated:
         if request.method == 'GET':
             logout(request)
             return redirect('/')
